24b4ea58675c6b9978178f654ad8070cb560fc4f
GitHub-hubgit <1075295452@qq.com>
2019-12-04 11:37:23 +0800

    '在模块的pom文件中添加了Junit依赖'

--- entity/src/main/java/com/iotek/entity/User.java
+++ entity/src/main/java/com/iotek/entity/User.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 /**
- * @Auther: 邪灵
+ * @Author: 邪灵
  * @Date: 2019/11/21 09:58
  * @Description: 用户类
  * @Since: 1.0
@@ -20,6 +20,16 @@ public class User implements Serializable {
     public User() {
     }
 
+    /**
+     * @date  2019/12/3 16:30
+     * @param id 主键属性
+     * @param name 姓名
+     * @param pass 密码
+     * @param age 年龄
+     * @param type 类型
+     * @return 
+     * @description 全参的构造方法
+     */
     public User(Integer id, String name, String pass, Byte age, Character type) {
         this.id = id;
         this.name = name;
@@ -28,6 +38,15 @@ public class User implements Serializable {
         this.type = type;
     }
 
+    /**
+     * @date  2019/12/3 16:31
+     * @param name 姓名
+     * @param pass 密码
+     * @param age 年龄
+     * @param type 类型
+     * @return 
+     * @description 除主键外属性构造
+     */
     public User(String name, String pass, Byte age, Character type) {
         this.name = name;
         this.pass = pass;
@@ -35,46 +54,112 @@ public class User implements Serializable {
         this.type = type;
     }
 
+    /**
+     * @date  2019/12/3 16:33
+     * @param 
+     * @return java.lang.Integer
+     * @description id属性的getter方法
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * @date  2019/12/3 16:33
+     * @param id
+     * @return void
+     * @description ID属性的setter方法
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * @date  2019/12/3 16:34
+     * @param 
+     * @return java.lang.String
+     * @description name属性的getter方法
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @date  2019/12/3 16:34
+     * @param name
+     * @return void
+     * @description name属性的setter方法
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @date  2019/12/3 16:34
+     * @param 
+     * @return java.lang.String
+     * @description pass属性的getter方法
+     */
     public String getPass() {
         return pass;
     }
 
+    /**
+     * @date  2019/12/3 16:34
+     * @param pass
+     * @return void
+     * @description pass属性的setter方法
+     */
     public void setPass(String pass) {
         this.pass = pass;
     }
 
+    /**
+     * @date  2019/12/3 16:35
+     * @param 
+     * @return java.lang.Byte
+     * @description age属性的getter方法
+     */
     public Byte getAge() {
         return age;
     }
 
+    /**
+     * @date  2019/12/3 16:35
+     * @param age
+     * @return void
+     * @description age属性的setter方法
+     */
     public void setAge(Byte age) {
         this.age = age;
     }
 
+    /**
+     * @date  2019/12/3 16:35
+     * @param 
+     * @return java.lang.Character
+     * @description type属性的getter方法
+     */
     public Character getType() {
         return type;
     }
 
+    /**
+     * @date  2019/12/3 16:35
+     * @param type
+     * @return void
+     * @description type属性的setter方法
+     */
     public void setType(Character type) {
         this.type = type;
     }
 
+    /**
+     * @date  2019/12/3 16:32
+     * @param o 任意对象
+     * @return boolean 是否相同
+     * @description 重写equals方法
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -87,11 +172,23 @@ public class User implements Serializable {
                 Objects.equals(type, user.type);
     }
 
+    /**
+     * @date  2019/12/3 16:33
+     * @param 
+     * @return int
+     * @description 重写hashcode方法
+     */
     @Override
     public int hashCode() {
         return Objects.hash(id, name, pass, age, type);
     }
 
+    /**
+     * @date  2019/12/3 16:33
+     * @param 
+     * @return java.lang.String
+     * @description 重写tostring方法
+     */
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("User").append(":[")
