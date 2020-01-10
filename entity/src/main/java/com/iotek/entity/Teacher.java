a152159d7681691aeb0289c7ffa6597b39ca8510
GitHub-hubgit <1075295452@qq.com>
2020-01-10 17:33:16 +0800

    添加了teacher类

---
+++ entity/src/main/java/com/iotek/entity/Teacher.java
@@ -0,0 +1,16 @@
+package com.iotek.entity;
+
+import lombok.Data;
+
+/**
+ * @Author: 邪灵
+ * @Date: 2020/1/10 17:31
+ * @Description: 教师类
+ * @Since: 1.0
+ */
+@Data
+public class Teacher {
+    private Integer id;
+    private String name;
+    private String pass;
+}
